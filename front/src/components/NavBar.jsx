d50e065b5063f65d6aac92d981c930030a7a8f4a
nyfeu <andre.maiolini@hotmail.com>
2025-04-28 19:23:12 -0300

    fix(auth-form): corrigir responsividade mobile

--- front/src/components/NavBar.jsx
+++ front/src/components/NavBar.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 const NavBar = () => {
     return (
-        <nav class="navbar navbar-light bg-light bg-dark px-3" style={{ height: '7vh' }}>
-            <a class="navbar-brand text-light justify-content-center m-0" href="#">Cookster</a>
+        <nav className="navbar navbar-light bg-light bg-dark px-3" style={{ height: '7vh', position: 'fixed',
+            top: 0,
+            left: 0,
+            right: 0,
+            zIndex: 1000 }}>
+            <a className="navbar-brand text-light justify-content-center m-0" href="#">Cookster</a>
+            <button className="btn btn-outline-light px-3">Entrar</button>
         </nav>
     )
 };
