9d8ea655c9d50ad2ae14d5c01e269f9f7b8fdcf4
nyfeu <andre.maiolini@hotmail.com>
2025-04-28 19:28:49 -0300

    feature(routes): criar roteamento entre as páginas do front

--- front/src/components/NavBar.jsx
+++ front/src/components/NavBar.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     return (
-        <nav className="navbar navbar-light bg-light bg-dark px-3" style={{ height: '7vh', position: 'fixed',
+        <nav className="navbar navbar-light bg-light bg-dark px-3" style={{
+            height: '7vh', position: 'fixed',
             top: 0,
             left: 0,
             right: 0,
-            zIndex: 1000 }}>
+            zIndex: 1000
+        }}>
             <a className="navbar-brand text-light justify-content-center m-0" href="#">Cookster</a>
-            <button className="btn btn-outline-light px-3">Entrar</button>
+            <div className='d-flex justify-content-end align-items-center'>
+                
+                <Link to="/" className="btn btn-outline-light mx-2">Home</Link>
+                <Link to="/login" className="btn btn-outline-light">Entrar</Link>
+            </div>
         </nav>
     )
 };
