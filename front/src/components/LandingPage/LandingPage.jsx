86a6301b4a9c6c1ad2fb04adaa7ad61bfba72851
nyfeu <andre.maiolini@hotmail.com>
2025-04-28 20:52:08 -0300

    feat(landing-page): criar landing-page

--- front/src/components/LandingPage/LandingPage.jsx
+++ front/src/components/LandingPage/LandingPage.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
 import NavBar from '../NavBar'
+import SocialMediaIcons from '../AuthForm/components/SocialMediaIcons'
+import './LandingPage.css'
 
 const LandingPage = () => {
-  return (
-    <div>
-        <NavBar/>
-    </div>
-  )
+    return (
+        <>
+            <div className='container-page'>
+                <NavBar />
+                <div className="container-fluid container-home p-0 d-flex flex-column">
+                    {/* Seção principal - conteúdo centralizado */}
+                    <div className="flex-grow-1 d-flex align-items-center justify-content-center">
+                        <div className="text-center px-4">
+                            <h1 className="display-4 mb-4">Welcome to Our Website</h1>
+                            <p className="lead mb-5">Discover amazing content and connect with us.</p>
+                            <button type="button" className="btn btn-custom px-4 py-3">
+                                Get Started
+                            </button>
+                        </div>
+                    </div>
+
+                    {/* Ícones de redes sociais - no final da página */}
+                    <div className="text-center py-4">
+                        <SocialMediaIcons />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default LandingPage
--- front/src/components/NavBar.jsx
+++ front/src/components/NavBar.jsx
@@ -3,8 +3,9 @@ import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     return (
-        <nav className="navbar navbar-light bg-light bg-dark px-3" style={{
-            height: '7vh', position: 'fixed',
+        <nav className="navbar fixed-top navbar-light bg-light bg-dark px-3" style={{
+            height: '7vh', 
+            position: 'fixed',
             top: 0,
             left: 0,
             right: 0,
@@ -12,7 +13,6 @@ const NavBar = () => {
         }}>
             <a className="navbar-brand text-light justify-content-center m-0" href="#">Cookster</a>
             <div className='d-flex justify-content-end align-items-center'>
-                
                 <Link to="/" className="btn btn-outline-light mx-2">Home</Link>
                 <Link to="/login" className="btn btn-outline-light">Entrar</Link>
             </div>
