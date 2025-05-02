ff1b681ab89b2c1f7dca552fcd6269ea6941d142
Lucas Paganotto <lucaspaga@hotmail.com>
2025-05-02 18:31:35 -0300

    feat(recipe-page): aprimora estrutura da pagina de receita

---
+++ front/src/components/RecipePage/componentes/IngredientsInfo.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const IngredientsInfo = () => {
+    return (
+        <div>
+            <h4>Ingredientes</h4>
+            <ul className="ingredient-list">
+                <li><p className="single-ingredient">1 1/2 cups dry pancake mix</p></li>
+                <li><p className="single-ingredient">1/2 cup flax seed meal</p></li>
+                <li><p className="single-ingredient">1 cup skim milk</p></li>
+            </ul>
+        </div>
+    )
+}
+
+export default IngredientsInfo
---
+++ front/src/components/RecipePage/componentes/ToolsInfo.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ToolsInfo = () => {
+    return (
+        <div>
+            <h4>Utensílios</h4>
+            <ul className="tool-list">
+                <li><p className="single-tool">Hand Blender</p></li>
+                <li><p className="single-tool">Large Heavy Pot With Lid</p></li>
+                <li><p className="single-tool">Measuring Spoons</p></li>
+                <li><p className="single-tool">Measuring Cups</p></li>
+            </ul>
+        </div>
+    )
+}
+
+export default ToolsInfo
