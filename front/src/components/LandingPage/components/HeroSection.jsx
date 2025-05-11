6b053bade9f7f55771c91178cfc595dbc30b0c01
nyfeu <andre.maiolini@hotmail.com>
2025-05-11 19:42:00 -0300

    feat(hero-section): cria a hero-section estilizada

--- front/src/components/LandingPage/components/HeroSection.jsx
+++ front/src/components/LandingPage/components/HeroSection.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./HeroSection.css";
 
 const HeroSection = () => {
     return (
-        <section id="hero-section" className="flex-grow-1 d-flex align-items-center justify-content-center">
-            <div className="text-center px-4">
-                <h1 className="display-4 mb-4">Bem-vindo ao Cookster</h1>
-                <p className="lead mb-5">Descubra conteúdos incríveis e conecte-se com uma comunidade apaixonada pela cozinha.</p>
-                <Link to='/login' type="button" className="btn btn-custom px-4 py-3">
-                    Começar Agora
-                </Link>
+        <section className="hero-banner py-5">
+            <div className="container py-3 d-flex flex-column flex-md-row align-items-center justify-content-center gap-5">
+                <div className="hero-text mb-4 mb-md-0 text-center">
+                    <h1 className="mb-3 hero-title">Porque cada ingrediente merece <br/> virar história</h1>
+                    <p className="my-4">
+                        Gerencie sua despensa, descubra receitas incríveis e compartilhe <br/> momentos que começam na sua cozinha.
+                    </p>
+                    <div>
+                        <button className="btn btn-custom text-white">Comece agora</button>
+                        <button className="btn btn-link text-dark">Explorar receitas</button>
+                    </div>
+                </div>
+                <div className="hero-image text-center mx-5">
+                    <img src='hero-banner.png' alt="Pessoa cozinhando" className="img-fluid" />
+                </div>
             </div>
         </section>
-    )
-}
+    );
+};
 
-export default HeroSection
+export default HeroSection;
