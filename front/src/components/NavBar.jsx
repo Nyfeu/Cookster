649f95763327edf508b51f4d03709c24098a075f
durval <durvalsoranz@gmail.com>
2025-05-31 20:40:09 -0300

    fix: ajusta cores e comportamento de ambas NavBars

--- front/src/components/NavBar.jsx
+++ front/src/components/NavBar.jsx
@@ -1,23 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FaUserCircle } from 'react-icons/fa'; // Ícone de perfil
 import './NavBar.css';
 
 const NavBar = () => {
+    const [isCollapsed, setIsCollapsed] = useState(true);
+
+    const toggleNavbar = () => {
+        setIsCollapsed(!isCollapsed);
+    };
+
+    const closeNavbar = () => {
+        setIsCollapsed(true);
+    };
+
     return (
         <nav className="navbar navbar-expand-lg custom-navbar px-4">
             <Link className="navbar-brand" to="/">Cookster</Link>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent">
+
+            <button className="navbar-toggler" type="button" onClick={toggleNavbar}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="var(--background-color)" viewBox="0 0 16 16">
                     <path fillRule="evenodd" d="M1.5 12a.5.5 0 0 1 0-1h13a.5.5 0 0 1 0 1h-13zm0-4a.5.5 0 0 1 0-1h13a.5.5 0 0 1 0 1h-13zm0-4a.5.5 0 0 1 0-1h13a.5.5 0 0 1 0 1h-13z" />
                 </svg>
             </button>
 
 
-            <div className="collapse navbar-collapse" id="navbarContent">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0 justify-content-lg-start justify-content-center align-items-center">
+            <div className={`collapse navbar-collapse ${isCollapsed ? '' : 'show'}`} id="navbarContent">
+                <ul className="navbar-nav mb-2 mb-lg-0">
                     <li className="nav-item">
-                        <Link className="nav-link" to="/">Home</Link>
+                        <Link className="nav-link" to="/" onClick={closeNavbar}>Home</Link>
                     </li>
                     <li className="nav-item">
                         <Link className="nav-link" to="/sobre">Sobre</Link>
@@ -26,7 +37,8 @@ const NavBar = () => {
                         <Link className="nav-link" to="/funcionalidades">Funcionalidades</Link>
                     </li>
                 </ul>
-                <div className="d-flex align-items-center  d-flex align-items-center gap-3 justify-content-center  flex-lg-row">
+
+                <div className="d-flex justify-content-start justify-content-lg-end w-100 px-1 gap-3">
                     <Link className="btn btn-outline-primary" to="/login" state={{ mode: 'sign_in' }}>Entrar</Link>
                     <Link className="btn btn-primary" to="/register" state={{ mode: 'sign_up' }}>Registrar-se</Link>
                 </div>
--- front/src/components/NavBar_Auth.jsx
+++ front/src/components/NavBar_Auth.jsx
@@ -1,48 +1,51 @@
-import React from 'react'
-import { useRef } from 'react';
-import { Link } from 'react-router-dom';
-import { FaUserCircle } from 'react-icons/fa'; // Ícone de perfil
+import React, { useState, useRef } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { FaUserCircle } from 'react-icons/fa';
 import './NavBar.css';
-import { TieredMenu } from 'primereact/tieredmenu';
-import { Button } from 'primereact/button';
-import { useNavigate } from 'react-router-dom'
 
 const NavBar_Auth = () => {
+    const [isCollapsed, setIsCollapsed] = useState(true);
+
+    const toggleNavbar = () => {
+        setIsCollapsed(!isCollapsed);
+    };
+
+    const closeNavbar = () => {
+        setIsCollapsed(true);
+    };
 
     return (
         <nav className="navbar navbar-expand-lg custom-navbar px-4">
             <Link className="navbar-brand" to="/">Cookster</Link>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent">
+
+            <button className="navbar-toggler" type="button" onClick={toggleNavbar}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="var(--background-color)" viewBox="0 0 16 16">
                     <path fillRule="evenodd" d="M1.5 12a.5.5 0 0 1 0-1h13a.5.5 0 0 1 0 1h-13zm0-4a.5.5 0 0 1 0-1h13a.5.5 0 0 1 0 1h-13zm0-4a.5.5 0 0 1 0-1h13a.5.5 0 0 1 0 1h-13z" />
                 </svg>
             </button>
 
-
-            <div className="collapse navbar-collapse" id="navbarContent">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0 justify-content-lg-start justify-content-center align-items-center">
+            <div className={`collapse navbar-collapse ${isCollapsed ? '' : 'show'}`} id="navbarContent">
+                <ul className="navbar-nav mb-2 mb-lg-0">
                     <li className="nav-item">
-                        <Link className="nav-link" to="/">Home</Link>
+                        <Link className="nav-link" to="/" onClick={closeNavbar}>Home</Link>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" to="/sobre">Despensa</Link>
+                        <Link className="nav-link" to="/sobre" onClick={closeNavbar}>Despensa</Link>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" to="/funcionalidades">Funcionalidades</Link>
+                        <Link className="nav-link" to="/funcionalidades" onClick={closeNavbar}>Funcionalidades</Link>
                     </li>
                 </ul>
-                <div className="d-flex align-items-center gap-3 d-flex align-items-center gap-3 justify-content-center  flex-lg-row">
-                                        <Link to="/profile"
-                                        >
-                                            <FaUserCircle style={{ color: "var(--background-color)" }} size={28} color="var(--background-color)" />
-                                        </Link>
 
-                    
-
+                <div className="d-flex justify-content-start justify-content-lg-end w-100 px-1">
+                    <Link to="/profile" onClick={closeNavbar} className="profile-icon">
+                        <FaUserCircle size={28} />
+                    </Link>
                 </div>
+
             </div>
         </nav>
-    )
-}
+    );
+};
 
-export default NavBar_Auth
+export default NavBar_Auth;
