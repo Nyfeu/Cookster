67ccaec4ad52030e45aebfe1a04ab7a9aadc84e5
Lucas Paganotto <lucaspaga@hotmail.com>
2025-06-03 10:18:50 -0300

    fix (mss-profile-service): remove funcao de autenticacao inutilizada no server.js

--- back/mss-profile-service/server.js
+++ back/mss-profile-service/server.js
@@ -28,24 +28,6 @@ app.use(cors({
 
 app.use(express.json());
 
-function checkAuthenticated(req, res, next) {
-  const authHeader = req.headers.authorization;
-
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    return res.status(401).json({ error: 'Token não fornecido' });
-  }
-
-  const token = authHeader.split(' ')[1];
-
-  try {
-    const decoded = jwt.verify(token, jwtSecret);
-    req.user = decoded; 
-    next();
-  } catch (err) {
-    return res.status(403).json({ error: 'Token inválido ou expirado' });
-  }
-}
-
 
 app.get('/profile/:userId', async (req, res) => {
     try {
