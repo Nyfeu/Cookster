6dfb1bd547dd60939326e56db259634f729ffcec
nyfeu <andre.maiolini@hotmail.com>
2025-10-12 19:05:14 -0300

    feat(mobile): adiciona tela de onboarding

---
+++ front_mobile/lib/theme/app_theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+// Cores baseadas no arquivo theme.css do seu projeto web
+class AppTheme {
+  static const Color primaryColor = Color(0xFF38302E);
+  static const Color secondaryColor = Color(0xFFE89841);
+  static const Color accentColor = Color(0xFF637F68);
+  static const Color backgroundColor = Color(0xFFFFF9EC);
+  static const Color textColor = Color(0xFF4D6B5B);
+  static const Color transitionColor = Color(0xFFABB8A5);
+}
